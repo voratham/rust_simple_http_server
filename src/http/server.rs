5218d1d8948da7cb960f8126e70f916c67d85870
voratham.s <voratham.s@linecorp.com>
2022-04-14 16:49:42 +0700

    init and learning build http_server from scratch

---
+++ src/http/server.rs
@@ -0,0 +1,42 @@
+use crate::http::HttpStatus;
+use crate::http::Method;
+use crate::http::Request;
+use crate::http::Response;
+use crate::http::Result;
+use std::convert::TryFrom;
+use std::io::Read;
+use std::net::TcpListener;
+
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
+    }
+    pub fn run(&self) -> Result<()> {
+        println!("Listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr)?; // try operator
+
+        for stream in listener.incoming() {
+            let mut stream = stream?;
+            let mut buf: [u8; 1024] = [0; 1024];
+            stream.read(&mut buf)?;
+            Request::try_from(&buf[..])?;
+            let request = Request::try_from(&buf[..])?;
+            println!("{:#?}", request);
+            let response = match request.method() {
+                Method::GET => match request.path().as_str() {
+                    "/" => Response::new(HttpStatus::OK, Some("home".to_string())),
+                    "/hello" => Response::new(HttpStatus::OK, Some("hello".to_string())),
+                    _ => Response::new(HttpStatus::BadRequest, None),
+                },
+                _ => Response::new(HttpStatus::OK, None),
+            };
+            response.send(&mut stream)?;
+        }
+
+        Ok(())
+    }
+}
